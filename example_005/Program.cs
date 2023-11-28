19b42c239adc2e55e43de00d9146b3706e8c5b89
Polina <marinini.po@gmail.com>
2023-11-28 22:25:29 +0300

    Ex_005

---
+++ example_005/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Masha");
+{
+    Console.WriteLine("Ура, это же МАША");
+}
+
+else
+{
+    Cosole.Write("Привет, ");
+    Console.WriteLine(username); 
+}
+
