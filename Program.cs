0de4f3a8974c3eb56c8fee507271c307b56f39ce
Polina <marinini.po@gmail.com>
2023-12-01 18:18:02 +0300

    Ex_011

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] collection)
+
+void PrintArray(int[] col)
+{
+    int length = collection.Length;
+    int position = 0;
+    while (position <)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
--- example_011/obj/Debug/net8.0/example_011.AssemblyInfo.cs
+++ example_011/obj/Debug/net8.0/example_011.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("example_011")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56b395f148dc4f31b41788dfa810c969dad064c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5168d833572413dcfe81ecba3f749f1cd7428e15")]
 [assembly: System.Reflection.AssemblyProductAttribute("example_011")]
 [assembly: System.Reflection.AssemblyTitleAttribute("example_011")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
